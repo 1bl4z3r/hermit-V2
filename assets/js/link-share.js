c340bdcb7ae397318a98bd2eada5baa35760c434
1bl4z3r <34515568+1bl4z3r@users.noreply.github.com>
2023-11-06 14:16:36 +0530

    Attempt(3) to Close #41

--- assets/js/link-share.js
+++ assets/js/link-share.js
@@ -1,6 +1,6 @@
 async function linkShare(t, u, s){
 let shd = {title: t,text: s,url: u};
-if(navigator.canShare(shd)){
+if(typeof navigator.canShare === "function" && navigator.canShare(shd)){
     try {await navigator.share(shd);} catch (er) {console.error(er);}
 }else if (navigator?.clipboard?.writeText){
     try {await navigator.clipboard.writeText(u);} catch (err) {console.error(err);} 
