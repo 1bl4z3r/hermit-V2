a6d5f768068e4197f28eecbed654a1766ee90fb4
GitHub Action <action@github.com>
2024-03-03 07:15:26 +0000

    Hermit-V2 Theme Update

---
+++ assets/js/mathjax-assistant.js
@@ -0,0 +1,12 @@
+MathJax = {
+  tex: {
+    displayMath: [['\\[', '\\]'], ['$$', '$$']],  // block
+    inlineMath: [['\\(', '\\)']]                  // inline
+      // processEscapes: true,
+      // processEnvironments: true,
+  }
+  // options: {
+  //     skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre'],
+  //     enableMenu: false
+  // }
+};
