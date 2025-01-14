3c5fe885223045639dc4e8fec1e7e855f199ac55
1bl4z3r <34515568+1bl4z3r@users.noreply.github.com>
2025-01-14 16:49:59 +0530

    .

--- assets/js/scrollwatcher.js
+++
@@ -1,7 +0,0 @@
-if (!(CSS.supports("animation-timeline: scroll()"))) {
-    const scroll = document.querySelector(".scroll-up"),rootElement = document.documentElement;
-    function handleScroll() {
-        rootElement.scrollTop / (rootElement.scrollHeight - rootElement.clientHeight) > .4 ? scroll.classList.add("show") : scroll.classList.remove("show");
-    }
-    document.addEventListener("scroll", handleScroll);
-}
